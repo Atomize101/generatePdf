ff91e3b964da8db80ab241128ade1820434fff7f
Chris Patrick <atomize101@gmail.com>
2020-12-17 09:15:05 -0500

    Setting up server code

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { saveAs } from 'file-saver';
 
 import './App.css';
 
-function App() {
+class App extends Component {
 	state = {
 		name: '',
 		receiptId: 0,
@@ -15,15 +15,21 @@ function App() {
 
 	handleChange = ({ target: { value, name } }) => this.setState({ [name]: value });
 
-	return (
-		<div className="App">
-			<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
-			<input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
-			<input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
-			<input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
-			<button onClick={this.createAndDownloadPdf}>Download PDF</button>
-		</div>
-	);
+	createAndDownloadPdf = () => {
+		axios.post('/create-pdf', this.state);
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
+				<input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
+				<input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
+				<input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
+				<button onClick={this.createAndDownloadPdf}>Download PDF</button>
+			</div>
+		);
+	}
 }
 
 export default App;
