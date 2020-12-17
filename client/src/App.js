4515af629b8946128fab968d2fa45c940766f3e3
Chris Patrick <atomize101@gmail.com>
2020-12-17 08:59:43 -0500

    Creating the handleChange function

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,22 @@ import { saveAs } from 'file-saver';
 import './App.css';
 
 function App() {
+	state = {
+		name: '',
+		receiptId: 0,
+		price1: 0,
+		price2: 0,
+	};
+
+	handleChange = ({ target: { value, name } }) => this.setState({ [name]: value });
+
 	return (
 		<div className="App">
 			<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
-			<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
-			<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
-			<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
-			<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
-			<input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
+			<input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
+			<input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
+			<input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
+			<button onClick={this.createAndDownloadPdf}>Download PDF</button>
 		</div>
 	);
 }
