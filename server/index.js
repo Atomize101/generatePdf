6ccd572249261bee7c03fbc438abe3d19b218f9d
Chris Patrick <atomize101@gmail.com>
2020-12-17 10:05:34 -0500

    create PDF post function

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
 
+const pdfTemplate = require('./documents');
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -10,4 +12,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// POST - PDF generation and fetching data
+
+app.post('/create-pdf', (req, res) => {
+	pdf.create(pdfTemplate(req.data), {}).toFile('result.pdf', (err) => {
+		if (err) {
+			return Promise.reject();
+		}
+
+		return Promise.resolve();
+	});
+});
+
+// GET - Send the generated PFD to client
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
